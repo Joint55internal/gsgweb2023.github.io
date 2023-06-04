14b902073035a5c6981a78c05799f4d71d2bf5da
Joint55internal <108395345+Joint55internal@users.noreply.github.com>
2023-06-04 17:18:14 +0900

    Update function.js

--- function.js
+++ function.js
@@ -1,25 +1,33 @@
 // Function to handle the touch event outside the dropdown menu
 function handleTouchOutside(event) {
-    const dropdowns = document.querySelectorAll('.dropdown-content');
-    dropdowns.forEach((dropdown) => {
-      if (!dropdown.parentNode.contains(event.target)) {
-        dropdown.style.display = 'none';
-      }
-    });
-  }
-  
-  // Event listener to detect touch events outside the dropdown menu
-  document.addEventListener('touchstart', handleTouchOutside, true);
-  
-  // Function to toggle the display of the dropdown menu
-  function toggleDropdown(event) {
-    const dropdown = event.currentTarget.nextElementSibling;
-    dropdown.style.display = (dropdown.style.display === 'block') ? 'none' : 'block';
-  }
-  
-  // Event listener to toggle the display of the dropdown menu when the button is clicked
-  const dropdownButtons = document.querySelectorAll('.dropbtn');
-  dropdownButtons.forEach((button) => {
-    button.addEventListener('click', toggleDropdown);
+  const dropdowns = document.querySelectorAll('.dropdown-content');
+  dropdowns.forEach((dropdown) => {
+    if (!dropdown.parentNode.contains(event.target)) {
+      dropdown.style.display = 'none';
+    }
   });
-  
+}
+
+// Event listener to detect touch events outside the dropdown menu
+document.addEventListener('touchstart', handleTouchOutside, true);
+
+// Function to toggle the display of the dropdown menu
+function toggleDropdown(event) {
+  const dropdown = event.currentTarget.nextElementSibling;
+  const isDisplayed = dropdown.style.display === 'block';
+
+  // Collapse all dropdown menus
+  const dropdowns = document.querySelectorAll('.dropdown-content');
+  dropdowns.forEach((dropdown) => {
+    dropdown.style.display = 'none';
+  });
+
+  // Toggle the display of the clicked dropdown menu
+  dropdown.style.display = isDisplayed ? 'none' : 'block';
+}
+
+// Event listener to toggle the display of the dropdown menu when the button is clicked
+const dropdownButtons = document.querySelectorAll('.dropbtn');
+dropdownButtons.forEach((button) => {
+  button.addEventListener('click', toggleDropdown);
+});
