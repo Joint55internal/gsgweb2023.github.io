ca1c3729ab5acf6b139ebe9ccdebe49af6757c5b
Joint55internal <108395345+Joint55internal@users.noreply.github.com>
2023-06-04 17:45:15 +0900

    Update function.js

--- function.js
+++ function.js
@@ -1,34 +1,31 @@
-// Function to handle the touch event outside the dropdown menu
-function handleTouchOutside(event) {
-  const dropdowns = document.querySelectorAll('.dropdown-content');
-  dropdowns.forEach((dropdown) => {
-    if (!dropdown.parentNode.contains(event.target)) {
-      dropdown.style.display = 'none';
+// Wait for the document to be fully loaded
+  document.addEventListener("DOMContentLoaded", function() {
+    // Get all the dropdown buttons
+    var dropdowns = document.getElementsByClassName("dropbtn");
+
+    // Add a click event listener to each dropdown button
+    for (var i = 0; i < dropdowns.length; i++) {
+      dropdowns[i].addEventListener("click", function() {
+        // Toggle the display of the corresponding dropdown content
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+          dropdownContent.style.display = "none";
+        } else {
+          dropdownContent.style.display = "block";
+        }
+      });
     }
+
+    // Close the dropdown menus if user clicks outside
+    window.addEventListener("click", function(event) {
+      if (!event.target.matches(".dropbtn")) {
+        var dropdownContents = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdownContents.length; i++) {
+          var openDropdown = dropdownContents[i];
+          if (openDropdown.style.display === "block") {
+            openDropdown.style.display = "none";
+          }
+        }
+      }
+    });
   });
-}
-
-// Event listener to detect click events outside the dropdown menu
-document.addEventListener('click', handleTouchOutside, true);
-
-// Event listener to detect touch events inside the dropdown menu and prevent propagation
-const dropdownContents = document.querySelectorAll('.dropdown-content');
-dropdownContents.forEach((dropdown) => {
-  dropdown.addEventListener('touchstart', function (event) {
-    event.stopPropagation();
-  });
-});
-
-// Function to toggle the display of the dropdown menu
-function toggleDropdown(event) {
-  const dropdown = event.currentTarget.nextElementSibling;
-  const isHidden = dropdown.style.display === 'none';
-  dropdown.style.display = isHidden ? 'block' : 'none';
-}
-
-// Event listener to toggle the display of the dropdown menu when the button is clicked or touched
-const dropdownButtons = document.querySelectorAll('.dropbtn');
-dropdownButtons.forEach((button) => {
-  button.addEventListener('click', toggleDropdown);
-  button.addEventListener('touchstart', toggleDropdown);
-});
