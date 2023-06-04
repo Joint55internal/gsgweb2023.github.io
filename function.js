da6348325ce2e9c555ff2e6ba13546d97d67651b
Joint55internal <108395345+Joint55internal@users.noreply.github.com>
2023-06-04 17:28:19 +0900

    Update function.js

--- function.js
+++ function.js
@@ -11,23 +11,18 @@ function handleTouchOutside(event) {
 // Event listener to detect click events outside the dropdown menu
 document.addEventListener('click', handleTouchOutside, true);
 
+// Event listener to detect touch events outside the dropdown menu
+document.addEventListener('touchstart', handleTouchOutside, true);
+
 // Function to toggle the display of the dropdown menu
 function toggleDropdown(event) {
   const dropdown = event.currentTarget.nextElementSibling;
-  const isDisplayed = dropdown.style.display === 'block';
-
-  // Collapse all dropdown menus
-  const dropdowns = document.querySelectorAll('.dropdown-content');
-  dropdowns.forEach((dropdown) => {
-    dropdown.style.display = 'none';
-  });
-
-  // Toggle the display of the clicked dropdown menu
-  dropdown.style.display = isDisplayed ? 'none' : 'block';
+  dropdown.style.display = (dropdown.style.display === 'block') ? 'none' : 'block';
 }
 
 // Event listener to toggle the display of the dropdown menu when the button is clicked
 const dropdownButtons = document.querySelectorAll('.dropbtn');
 dropdownButtons.forEach((button) => {
   button.addEventListener('click', toggleDropdown);
+  button.addEventListener('touchstart', toggleDropdown);
 });
