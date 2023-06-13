bbec8c225c9a2897b8901496e957d0019cef0993
Joint55internal <108395345+Joint55internal@users.noreply.github.com>
2023-06-14 02:29:14 +0900

    line share

--- share.js
+++ share.js
@@ -1,14 +1,14 @@
 function shareViaLine() {
     var currentURL = encodeURIComponent(window.location.href);
     var message = "Check out this page: " + currentURL;
-    var lineShareURL = "https://line.me/R/msg/text/" + encodeURIComponent(message);
+    var lineShareURL = "https://line.me/R/msg/text/?" + (message);
     window.open(lineShareURL, "_blank");
   }
   // Function to share via WhatsApp
 function shareViaWhatsApp() {
     var currentURL = encodeURIComponent(window.location.href);
     var message = "Check out this page: " + currentURL;
-    var whatsappShareURL = "https://wa.me/?text=" + encodeURIComponent(message);
+    var whatsappShareURL = "https://wa.me/?text=" + (message);
     window.open(whatsappShareURL, "_blank");
   }
   // Attach event listeners to the sharing buttons
